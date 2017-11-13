eab1c52a1977e59e354f396b9f810e1ed91639e7
relentless-coder <contact@ayushbahuguna.com>
2017-11-14 03:19:23 +0530

    add a stock property

--- services/layers/product.layer.js
+++ services/layers/product.layer.js
@@ -1,31 +1,34 @@
 export class getProduct{
-    constructor({_id, title, description, category, price, gallery}){
-        this._id = id;
-        this.title = title;
-        this.description = description;
-        this.category = category;
-        this.price = price;
-        this.gallery = gallery;
-    }
-}
-
-export class addProduct{
-    constructor({title, description, category, price, gallery}){
-        this.title = title;
-        this.description = description;
-        this.category = category;
-        this.price = price;
-        this.gallery = gallery;
-    }
-}
-
-export class updateProduct{
-    constructor({_id, title, description, category, price, gallery}){
+    constructor({_id, title, description, category, price, gallery, stock}){
         this._id = _id;
         this.title = title;
         this.description = description;
         this.category = category;
         this.price = price;
+        this.stock = stock;
+        this.gallery = gallery;
+    }
+}
+
+export class addProduct{
+    constructor({title, description, category, price, gallery, stock}){
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+        this.stock = stock;
+        this.gallery = gallery;
+    }
+}
+
+export class updateProduct{
+    constructor({_id, title, description, category, price, gallery, stock}){
+        this._id = _id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+        this.stock = stock;
         this.gallery = gallery;
     }
 }
