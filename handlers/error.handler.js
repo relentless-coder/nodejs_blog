12e24aa4f8817552a304ae6cd028c40f9fbeba0c
relentless-coder <contact@ayushbahuguna.com>
2017-11-14 03:18:39 +0530

    extend upon the Error class to allow code and error

---
+++ handlers/error.handler.js
@@ -0,0 +1,9 @@
+class ErrorWithStatusCode extends Error{
+    constructor(code, message, err){
+        super(message);
+        this.code = code;
+        this.error = err;
+    }
+}
+
+export {ErrorWithStatusCode}
