aefb1473c8f733234fc5245bb9b8c35c886266f9
relentless-coder <contact@ayushbahuguna.com>
2017-11-16 18:40:10 +0530

    reformat with 2 tab indentation

--- config/mongoconfig.js
+++ config/mongoconfig.js
@@ -4,17 +4,17 @@ const url = 'mongodb://localhost:27017/wingify-project';
 
 const client = mongodb.MongoClient;
 
-const connectMongo = (dummy)=>{
-    if(dummy) return Promise.resolve(dummy);
-    return new Promise((resolve, reject)=>{
-        client.connect(url, (err, db)=>{
-            if(err){
-                reject(err)
-            } else {
-                resolve(db)
-            }
-        })
+const connectMongo = (dummy) => {
+  if (dummy) return Promise.resolve(dummy);
+  return new Promise((resolve, reject) => {
+    client.connect(url, (err, db) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(db)
+      }
     })
+  })
 };
 
 export {connectMongo}
--- config/multerconfig.js
+++ config/multerconfig.js
@@ -2,19 +2,19 @@ import multer from 'multer';
 import path from 'path';
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        if(!file){
-            return
-        }
-        cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-        if(!file){
-            return;
-        }
-        let ext = path.extname(file.originalname);
-        cb(null, file.fieldname + '-' + Date.now() + ext)
+  destination: (req, file, cb) => {
+    if (!file) {
+      return
     }
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    if (!file) {
+      return;
+    }
+    let ext = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + Date.now() + ext)
+  }
 });
 
 export const upload = multer({storage});
