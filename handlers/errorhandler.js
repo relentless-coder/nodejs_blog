671a115d65b14f4304b5baf3c45c24eb02bf0bc5
relentless-coder <contact@ayushbahuguna.com>
2017-11-16 18:40:15 +0530

    reformat with 2 tab indentation

--- handlers/errorhandler.js
+++ handlers/errorhandler.js
@@ -1,9 +1,9 @@
-class ErrorWithStatusCode extends Error{
-    constructor(code, message, err){
-        super(message);
-        this.code = code;
-        this.error = err;
-    }
+class ErrorWithStatusCode extends Error {
+  constructor(code, message, err) {
+    super(message);
+    this.code = code;
+    this.error = err;
+  }
 }
 
 export {ErrorWithStatusCode}
