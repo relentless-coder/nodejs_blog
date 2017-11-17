527f65469247149c0a3db67f2dd7603706d8950c
relentless-coder <contact@ayushbahuguna.com>
2017-11-17 09:59:43 +0530

    for single queries set ObjectID

--- modules/product/product.controllers.js
+++ modules/product/product.controllers.js
@@ -1,123 +1,139 @@
-import {findAll, findSingle, insert, update} from "../../services/mongodb/mongodb.service";
+import {findAll, findSingle, insert, update, removeOne} from "../../services/mongodb/mongodb.service";
 import {getProduct, addProduct, updateProduct} from "../../services/layers/product.layer";
 import {upload} from "../../config/multerconfig";
+import {ObjectID} from 'mongodb';
 import {ErrorWithStatusCode} from "../../handlers/errorhandler";
+
 
 const fileUpload = upload.array('gallery')
 
 export function getAllProducts(req, res) {
-  findAll('products', {}, getProduct).then((data) => {
-    console.log(data);
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({data}))
-  }).catch((err) => {
-    res.writeHead(500, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({data}))
-  })
+    findAll('products', {}, getProduct).then((data) => {
+        console.log(data);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({data}))
+    }).catch((err) => {
+        res.writeHead(500, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({data}))
+    })
 }
 
 export function getOneProduct(req, res) {
-  let query = {
-    _id: req.params.productId
-  };
-  findSingle('productions', query, getProduct).then((data) => {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({data}))
-  })
+    console.log(req.params.productId)
+    let query = {
+        '_id': ObjectID(req.params.productId)
+    };
+    findSingle('products', query, getProduct).then((data) => {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(data))
+    })
 }
 
 export function addOneProduct(req, res) {
 
-  const getData = () => {
-    return new Promise((resolve, reject) => {
-      fileUpload(req, res, (err) => {
-        if (err) {
-          console.log(err);
-          reject(err)
+    const getData = () => {
+        return new Promise((resolve, reject) => {
+            fileUpload(req, res, (err) => {
+                if (err) {
+                    console.log(err);
+                    reject(err)
+                } else {
+                    let files = [];
+                    if (req.files) {
+                        req.files.forEach((file) => {
+                            files.push(file.path);
+                        });
+                        req.body.gallery = files;
+                    }
+
+                    resolve(req)
+
+                }
+            })
+        })
+
+    };
+
+    const addToDatabase = () => {
+        return insert('products', req.body, addProduct, getProduct).then((data) => {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(data))
+        }).catch((err) => {
+            throw new ErrorWithStatusCode(500, 'Sorry, we seem to be facing some issue right now. Please try again later.', err);
+        })
+    };
+
+    getData().then(addToDatabase).catch((err) => {
+        if (err.code && err.message) {
+            res.writeHead(err.code, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({message: err.message, error: err.error}))
         } else {
-          let files = [];
-          if (req.files) {
-            req.files.forEach((file) => {
-              files.push(file.path);
-            });
-            req.body.gallery = files;
-          }
-
-          resolve(req)
-
+            res.writeHead(500, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({
+                message: 'Sorry, we seem to be facing some issue right now. Please, try again later.',
+                error: err
+            }))
         }
-      })
     })
-
-  };
-
-  const addToDatabase = () => {
-    return insert('products', req.body, addProduct, getProduct).then((data) => {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify(data))
-    }).catch((err) => {
-      throw new ErrorWithStatusCode(500, 'Sorry, we seem to be facing some issue right now. Please try again later.', err);
-    })
-  };
-
-  getData().then(addToDatabase).catch((err) => {
-    if (err.code && err.message) {
-      res.writeHead(err.code, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({message: err.message, error: err.error}))
-    } else {
-      res.writeHead(500, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({
-        message: 'Sorry, we seem to be facing some issue right now. Please, try again later.',
-        error: err
-      }))
-    }
-  })
 }
 
 export function updateOneProduct(req, res) {
 
-  const getData = () => {
-    return new Promise((resolve, reject) => {
-      fileUpload(req, res, (err) => {
-        if (err) {
-          console.log("upload error ", err);
-          reject(err)
+    const getData = () => {
+        return new Promise((resolve, reject) => {
+            fileUpload(req, res, (err) => {
+                if (err) {
+                    console.log("upload error ", err);
+                    reject(err)
+                } else {
+                    if (req.files) {
+                        console.log('we have files', req.files);
+                        req.files.forEach((file) => {
+                            req.body.gallery.push(file.path);
+                        });
+                    }
+                    resolve(req)
+                }
+
+            })
+        })
+
+    };
+
+    const addToDatabase = () => {
+        console.log(req.body)
+        return update('products', {_id: req.params.productId}, req.body, updateProduct, getProduct).then((data) => {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(data))
+        }).catch((err) => {
+            console.log("Error is in add to database ", err);
+            throw new ErrorWithStatusCode(500, 'Sorry, we seem to be facing some issue right now. Please try again later.', err);
+        })
+    };
+
+    getData().then(addToDatabase).catch((err) => {
+        if (err.code && err.message) {
+            res.writeHead(err.code, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({message: err.message, error: err.error}))
         } else {
-          if (req.files) {
-            console.log('we have files', req.files);
-            req.files.forEach((file) => {
-              req.body.gallery.push(file.path);
-            });
-          }
-          resolve(req)
+            res.writeHead(500, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({
+                message: 'Sorry, we seem to be facing some issue right now. Please, try again later.',
+                error: err
+            }))
         }
-
-      })
     })
+}
 
-  };
+export function removeOneProduct(req, res) {
+    let query = {
+        _id: ObjectID(req.params.productId)
+    };
 
-  const addToDatabase = () => {
-    console.log(req.body)
-    return update('products', {_id: req.params.productId}, req.body, updateProduct, getProduct).then((data) => {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify(data))
+    removeOne('products', query).then((data) => {
+        console.log('remove one data is')
+        res.writeHead(data.status, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(data))
     }).catch((err) => {
-      console.log("Error is in add to database ", err);
-      throw new ErrorWithStatusCode(500, 'Sorry, we seem to be facing some issue right now. Please try again later.', err);
     })
-  };
-
-  getData().then(addToDatabase).catch((err) => {
-    if (err.code && err.message) {
-      res.writeHead(err.code, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({message: err.message, error: err.error}))
-    } else {
-      res.writeHead(500, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({
-        message: 'Sorry, we seem to be facing some issue right now. Please, try again later.',
-        error: err
-      }))
-    }
-  })
 }
