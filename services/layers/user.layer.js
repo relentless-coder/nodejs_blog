650acda7ecca57ee5a971c8c3689bcf6245d278f
relentless-coder <contact@ayushbahuguna.com>
2017-11-18 06:50:58 +0530

    setJwt, parseUser, hashPassword classes

---
+++ services/layers/user.layer.js
@@ -0,0 +1,41 @@
+import {encode} from "../jwt/jwt.encode";
+import crypto from 'crypto';
+import {cryptoConfig} from "../../config/crypto.config";
+
+class setJwt {
+  constructor({_id}){
+    this._id = _id;
+    this.token = setJwt.generateToken({_id: _id})
+  }
+
+  static generateToken(payload){
+      return encode(payload)
+  }
+
+}
+
+class parseUser {
+  constructor({_id, email}){
+    this._id = _id;
+    this.email = email;
+  }
+}
+
+class hashPassword {
+  constructor({email, password}){
+    console.log(email, password);
+    this.email = email
+    this.salt = hashPassword.generateSalt();
+    this.password = hashPassword.generateHash(password, this.salt, cryptoConfig.iterations)
+  }
+
+  static generateSalt(){
+    return crypto.randomBytes(256).toString('base64')
+  }
+
+  static generateHash(password, salt, iterations){
+    return crypto.pbkdf2Sync(password, salt, iterations, 32, 'sha512').toString('base64')
+  }
+}
+
+export {setJwt, hashPassword, parseUser}
