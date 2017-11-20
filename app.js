56c967a666d3bbba413f371a7dbf813b2d337f5a
relentless-coder <contact@ayushbahuguna.com>
2017-11-21 00:50:40 +0530

    format with tab size 2

--- app.js
+++ app.js
@@ -2,22 +2,28 @@ import http from 'http';
 import Router from 'router';
 import {productRouter} from "./modules/product/product.routes";
 import {userRouter} from "./modules/user/user.routes";
+import {authHandler} from './handlers/auth.handler';
 
 const router = new Router();
+
+router.use(authHandler);
+
+
 
 router.use('/api/v1', productRouter);
 
 router.use('/api/v1', userRouter);
 
-const server = http.createServer((req, res)=>{
-    router(req, res, handleError)
+
+const server = http.createServer((req, res) => {
+  router(req, res, handleError)
 });
 
 
 function handleError(data) {
-    console.log("data is ", data);
+  console.log("data is ", data);
 }
 
-server.listen(5000, ()=>{
-    console.log('Server listening on port 5000')
+server.listen(5000, () => {
+  console.log('Server listening on port 5000')
 });
