a307e5a6902842a69f0423e75ce6ab754100cfba
relentless-coder <contact@ayushbahuguna.com>
2017-11-21 01:01:11 +0530

    vaidate password before setting jwt token

--- services/layers/user.layer.js
+++ services/layers/user.layer.js
@@ -1,39 +1,53 @@
 import {encode} from "../jwt/jwt.encode";
 import crypto from 'crypto';
 import {cryptoConfig} from "../../config/crypto.config";
+import {ErrorWithStatusCode} from '../../handlers/errorhandler';
+import {responseHandler} from '../../handlers/response.handler';
 
 class setJwt {
-  constructor({_id}){
+  constructor({_id, password, salt, userPassword}) {
+    if(!_id){
+      throw new ErrorWithStatusCode(422, '_id not found', 'to create a jwt token, valid value of ')
+    }
+    if(!setJwt.compareHash(password, userPassword, salt, cryptoConfig.iterations)){
+      throw new ErrorWithStatusCode(401, 'invalid password', 'The password didn\'t match')
+    }
     this._id = _id;
-    this.token = setJwt.generateToken({_id: _id})
+    this.token = setJwt.generateToken({_id: _id});
   }
 
-  static generateToken(payload){
-      return encode(payload)
+  static generateToken(payload) {
+    return encode(payload)
   }
 
+  static compareHash(password, userPassword, salt, iterations){
+    return password === hashPassword.generateHash(userPassword, salt, iterations);
+  }
 }
 
 class parseUser {
-  constructor({_id, email}){
+  constructor({_id, email}) {
+    console.log(_id, email);
     this._id = _id;
     this.email = email;
   }
 }
 
 class hashPassword {
-  constructor({email, password}){
-    console.log(email, password);
-    this.email = email
+  constructor({email, password}) {
+    if(!email || !password){
+      throw new ErrorWithStatusCode(422, 'Email or password not found.', 'hashPassword class requires email and password in the object.')
+    }
+    this.email = email;
     this.salt = hashPassword.generateSalt();
     this.password = hashPassword.generateHash(password, this.salt, cryptoConfig.iterations)
   }
 
-  static generateSalt(){
+  static generateSalt() {
     return crypto.randomBytes(256).toString('base64')
   }
 
-  static generateHash(password, salt, iterations){
+  static generateHash(password, salt, iterations) {
     return crypto.pbkdf2Sync(password, salt, iterations, 32, 'sha512').toString('base64')
   }
 }
