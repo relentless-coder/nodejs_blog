bfdfb73a3045af4e18ed1d26f977b3c038f039a3
relentless-coder <contact@ayushbahuguna.com>
2017-11-25 00:12:34 +0530

    don't let user enter an empty object, or update with incomplete object

--- services/layers/product.layer.js
+++ services/layers/product.layer.js
@@ -1,3 +1,5 @@
+import {ErrorWithStatusCode} from '../../handlers/errorhandler';
+
 export class getProduct {
   constructor({_id, title, description, category, price, gallery, stock}) {
     this._id = _id;
@@ -12,24 +14,31 @@ export class getProduct {
 
 export class addProduct {
   constructor({title, description, category, price, gallery, stock}) {
-    this.title = title;
-    this.description = description;
-    this.category = category;
-    this.price = Number(price);
-    this.stock = Number(stock);
-    this.gallery = gallery;
+    if(!title || !description || !category || !price || !gallery || !stock){
+      throw new ErrorWithStatusCode(422, 'Sorry, can\'t process request without complete data.', 'The data received from the client is incomplete. Required are title, description, category, price, gallery, stock.')
+    } else {
+      this.title = title;
+      this.description = description;
+      this.category = category;
+      this.price = Number(price);
+      this.stock = Number(stock);
+      this.gallery = gallery;
+    }
   }
 }
 
 export class updateProduct {
   constructor({_id, title, description, category, price, gallery, stock}) {
-
-    this._id = _id;
-    this.title = title;
-    this.description = description;
-    this.category = category;
-    this.price = Number(price);
-    this.stock = Number(stock);
-    this.gallery = gallery;
+    if(!_id || !title || !description || !category || !price || !gallery || !stock){
+      throw new ErrorWithStatusCode(422, 'Can\'t update document with empty object', 'Send the object with all the properties, as the missing keys would replace the original keys with null values.')
+    } else {
+      this._id = _id;
+      this.title = title;
+      this.description = description;
+      this.category = category;
+      this.price = Number(price);
+      this.stock = Number(stock);
+      this.gallery = gallery;
+    }
   }
 }
