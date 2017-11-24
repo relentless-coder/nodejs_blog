966da35ad80e29ccf4bcc9fd9c6c942980eb09df
relentless-coder <contact@ayushbahuguna.com>
2017-11-25 00:08:27 +0530

    simulate a callback with the insertOne dummy, and pass it to the Promise.resolve

--- test/unit/services/mongodb/mongodb.service.spec.js
+++ test/unit/services/mongodb/mongodb.service.spec.js
@@ -7,76 +7,83 @@ import {ErrorWithStatusCode} from "../../../../handlers/errorhandler";
 
 const expect = chai.expect;
 
-describe('Mongodb insert', ()=>{
-    it('should return 422 when called with empty document', ()=>{
-        let doc = {};
+describe('Mongodb insert', () => {
+  it('should return 422 when called with empty document', () => {
+    let doc = {};
 
-        let collection = {
-            insertOne: (doc)=> {
-                doc._id = '123absd';
-                return Promise.resolve({
-                    ops: doc
-                })
-            }
-        };
+    let collection = {
+      insertOne: (doc) => {
+        doc._id = '123absd';
+        return Promise.resolve({
+          ops: doc
+        })
+      }
+    };
 
-        let dummy = {
-            collection: (collection)=>{
-                return collection
-            },
-            close: ()=>{
-                return true;
-            }
-        };
+    let dummy = {
+      collection: (collection) => {
+        return collection
+      },
+      close: () => {
+        return true;
+      }
+    };
 
-        let spy = sinon.spy(collection, 'insertOne');
+    let spy = sinon.spy(collection, 'insertOne');
+    expect(()=>{
+      insert(collection, doc, addProduct, getProduct, dummy)
+    }).to.throw().with.property('code', 400);
+  });
 
-        expect(()=>{insert(collection, doc, addProduct, getProduct, dummy)}).to.throw('Can\'t process request with incomplete data.').with.property('code', 422);
+
+  it('should call the right methods on insert and insert a document', (done)=>{
+    let doc = {
+      title: 'Product 1',
+      price: 2231,
+      stock: 20,
+      tax: 18,
+      description: 'This is a fine first product, for sure',
+      gallery: ['image1.png', 'image2.png'],
+      category: 'clothing'
+    };
+
+    let cb = (err, data)=>{
+      return {ops: data}
+    };
+
+    let collection = {
+      insertOne: (doc, cb)=>{
+        doc._id = '123absd';
+        let data = {ops: [doc]};
+        return Promise.resolve(cb(null, data))
+      }
+    };
+
+    let dummy = {
+      collection: (collection)=>{
+        return collection
+      },
+      close: ()=>{
+        return true;
+      }
+    };
+
+    let spy1 = sinon.spy(dummy, 'collection');
+    let spy2 = sinon.spy(dummy, 'close');
+    let spy3 = sinon.spy(collection, 'insertOne');
+
+    insert(collection, doc, addProduct, getProduct, dummy).then((data)=>{
+      expect(spy1.calledOnce).to.be.true;
+      expect(spy2.calledOnce).to.be.true;
+      expect(spy3.calledOnce).to.be.true;
+      data.should.be.a('object');
+      data.should.not.have.property('error');
+      data['status'].should.equal(200);
+      data['data'].should.have.property('_id');
+      data['data'].should.not.have.property('tax');
+      done()
     });
 
-    it('should call the right methods on insert and insert a document', (done)=>{
-        let doc = {
-            title: 'Product 1',
-            price: 2231,
-            stock: 20,
-            tax: 18,
-            category: 'clothing'
-        };
-
-        let collection = {
-            insertOne: (doc)=>{
-                doc._id = '123absd';
-                return Promise.resolve({
-                    ops: doc
-                })
-            }
-        };
-
-        let dummy = {
-            collection: (collection)=>{
-                return collection
-            },
-            close: ()=>{
-                return true;
-            }
-        };
-
-        let spy1 = sinon.spy(dummy, 'collection');
-        let spy2 = sinon.spy(dummy, 'close');
-        let spy3 = sinon.spy(collection, 'insertOne');
-
-        insert(collection, doc, addProduct, getProduct, dummy).then((data)=>{
-            expect(spy1.calledOnce).to.be.true;
-            expect(spy2.calledOnce).to.be.true;
-            expect(spy3.calledOnce).to.be.true;
-            data.should.be.a('object');
-            data.should.not.have.property('error');
-            data['status'].should.equal(200);
-            data['data'].should.have.property('_id');
-            data['data'].should.not.have.property('tax');
-            done()
-        });
-
-    })
+  })
 });
 
