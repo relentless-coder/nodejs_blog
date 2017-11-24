87d4b2d3d360e23a15ad4cd75f14347fd12820ba
relentless-coder <contact@ayushbahuguna.com>
2017-11-25 00:10:20 +0530

    test all the CRUD operations on product

---
+++ test/unit/modules/product/product.controller.spec.js
@@ -0,0 +1,143 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import {addOneProduct, updateOneProduct} from '../../../../modules/product/product.controllers';
+import {removeOne} from '../../../../services/mongodb/mongodb.service';
+
+const expect = chai.expect;
+
+describe('Add Product', ()=>{
+
+  let productId;
+
+  after((done)=>{
+    removeOne('products', {title: 'Test Product'}).then(data => done()).catch((err)=>{
+      throw err
+    });
+  });
+
+  it('should return with 422 for incomplete data', (done)=>{
+    let req = {
+      body: {
+        title: 'Test Product',
+        price: 22139,
+        stock: 22
+      }
+    };
+
+    let res = {
+      writeHead: (code, obj)=>{
+        return true
+      },
+      end: (msg)=>{
+        return true
+      }
+    };
+
+    let spy = sinon.spy(res, 'writeHead');
+
+    addOneProduct(req, res).then((data)=>{
+      expect(spy.withArgs(422, {'Content-Type': 'application/json'}).calledOnce).to.be.true;
+      done()
+    })
+
+  });
+
+  it('should successfully insert a product', (done)=>{
+      let req = {
+        body: {
+          title: 'Test Product',
+          price: 2212,
+          stock: 20,
+          description: 'This is a fine test product',
+          gallery: ['image.png', 'test.png'],
+          category: 'test'
+        }
+      };
+
+    let res = {
+      writeHead: (code, obj)=>{
+        return true
+      },
+      end: (msg)=>{
+        return true
+      }
+    };
+
+    let spy = sinon.spy(res, 'writeHead');
+
+    addOneProduct(req, res).then((data)=>{
+      expect(spy.withArgs(200, {'Content-Type': 'application/json'}).calledOnce).to.be.true;
+      expect(data).to.have.property('_id');
+      productId = data._id;
+      done()
+    })
+
+  });
+
+  it('should return with 422 when updating with incomplete object', (done)=>{
+    let req = {
+      body:{
+        title: 'Test Product',
+        price: 2212,
+        description: 'This is a fine test product',
+        gallery: ['image.png', 'test.png'],
+        category: 'test',
+        _id: productId
+      },
+      params: {
+        productId
+      }
+    };
+
+    let res = {
+      writeHead: (code, obj)=>{
+        return true
+      },
+      end: (msg)=>{
+        return true
+      }
+    };
+
+    let spy = sinon.spy(res, 'writeHead');
+
+    updateOneProduct(req, res).then((data)=>{
+      expect(spy.withArgs(422, {'Content-Type': 'application/json'}).calledOnce).to.be.true;
+      done()
+    })
+  });
+
+  it('should successfully update a product', (done)=>{
+    let req = {
+      body:{
+        title: 'Test Product',
+        price: 2212,
+        stock: 25,
+        description: 'This is a fine test product',
+        gallery: ['image.png', 'test.png'],
+        category: 'test',
+        _id: productId
+      },
+      params: {
+        productId
+      }
+    };
+
+    let res = {
+      writeHead: (code, obj)=>{
+        return true
+      },
+      end: (msg)=>{
+        return true
+      }
+    };
+
+    let spy = sinon.spy(res, 'writeHead');
+
+    updateOneProduct(req, res).then((data)=>{
+      expect(spy.withArgs(200, {'Content-Type': 'application/json'}).calledOnce).to.be.true;
+      expect(data.stock).to.equal(25);
+      done()
+    })
+
+  })
+});
