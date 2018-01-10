d89df25ca4475e7ecc2bfe1a02fe17852d9d1f90
relentless-coder <contact@ayushbahuguna.com>
2018-01-10 22:15:36 +0530

    files upload handler

---
+++ server/handlers/upload.handler.js
@@ -0,0 +1,29 @@
+import {upload} from '../config/multer.config';
+
+
+function uploadHandler() {
+  return (req, res, key) => {
+    const uploadName = upload.file(key)
+
+    return new Promise((resolve, reject) => {
+      uploadName(req, (err) => {
+        if (err)
+          reject(err)
+        else {
+          let files = [];
+          if (req.files) {
+            req.files.forEach((file) => {
+              files.push(file.path);
+            });
+            req.body.gallery = files;
+          }
+
+          resolve(req)
+        }
+
+      })
+    })
+  }
+}
+
+export default uploadHandler()
