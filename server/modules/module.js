fa8fedd50e775b858eb7b57a0a52ac640d656f0e
relentless-coder <contact@ayushbahuguna.com>
2018-01-10 22:13:23 +0530

    setup all routes and serve static files

---
+++ server/modules/module.js
@@ -0,0 +1,18 @@
+import Router from 'router'
+import {userRouter} from './user/user.routes';
+import {postRouter} from './post/post.routes';
+import {serveStatic} from '../handlers/serve.static';
+
+const router = new Router();
+
+function routerFactory() {
+
+  router.use('/post', postRouter);
+  router.use('/user', userRouter);
+
+  router.get('*', serveStatic);
+
+  return router
+}
+
+export default routerFactory()
