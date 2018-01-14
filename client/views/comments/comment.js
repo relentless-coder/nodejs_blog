f0dce9e28eb308a951bf9e2cc311f54608ea80db
relentless-coder <contact@ayushbahuguna.com>
2018-01-14 23:15:46 +0530

    generate object out of the form

--- client/views/comments/comment.js
+++ client/views/comments/comment.js
@@ -1,14 +1,31 @@
 import commentFactory from './comment.factory'
 
 function comment() {
-    const postComment = (comment, url)=>{
-      commentFactory.postComment(comment, url)
+  const postComment = (url) => {
+    console.log(document.getElementById('comment_name').value, document.getElementById('comment_email').value, document.getElementById('author_new_comment').value)
+    const data = {
+      author: {
+        name: document.getElementById('comment_name').value,
+        email: document.getElementById('comment_email').value
+      },
+      comment: document.getElementById('author_new_comment').value
     }
+    commentFactory.postComment(data, url).then((data)=>{
+      const commentWrapper = document.querySelector('.comment_wrapper');
+      const result = Handlebars.templates['comment'](data.data);
+      console.log(result)
+      commentWrapper.insertAdjacentHTML('beforeend', result);
+    })
+  }
 
-    const replyComment = (comment, id)=>{
-      commentFactory.replyComment(comment, id)
-    }
+  const replyComment = (comment, id) => {
+    commentFactory.replyComment(comment, id)
+  }
 
+  return {
+    postComment, replyComment
+  }
 }
 
 export {comment}
+
