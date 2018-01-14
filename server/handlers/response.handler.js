afd16968fe4fdcaee00e80d844b8a6935b5cb8f2
relentless-coder <contact@ayushbahuguna.com>
2018-01-14 23:11:39 +0530

    if the data is an object, then stringify it

--- server/handlers/response.handler.js
+++ server/handlers/response.handler.js
@@ -1,15 +1,16 @@
-import {ErrorWithStatusCode} from './errorhandler';
-
 function responseHandler(res, options) {
   if(!(Object.keys(options).length >= 4)){
     throw SyntaxError('You need to pass atleast 4 arguments');
   } else if (typeof arguments[0] !== 'object' || !arguments[0].writeHead || !arguments[0].end) {
     throw TypeError('The first argument must be an object with methods writeHead and end');
   } else {
-
     res.writeHead(options.status, {'Content-Type': options.content});
-    res.end(options.data);
-    console.log("dne");
+    let data;
+    if(typeof options.data === 'object')
+      data = JSON.stringify(options.data);
+    else
+      data = options.data;
+    res.end(data);
     return true
   }
 }
