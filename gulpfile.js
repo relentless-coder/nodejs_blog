b7578a4f259fffc5e7b71dd5152195a2089a80c4
relentless-coder <contact@ayushbahuguna.com>
2018-01-23 23:06:38 +0530

    set indentation

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,22 @@ const concat = require('gulp-concat');
 
 
 gulp.task('clean', ()=>{
-  return gulp.src(['./client/blog/build/*.js', './client/blog/admin/*.js'], {read: false})
-    .pipe(clean())
-})
+	return gulp.src(['./client/blog/build/*.js', './client/blog/admin/*.js'], {read: false})
+		.pipe(clean());
+});
 
 
 gulp.task('blog-sass', ['clean'], ()=>{
-  return gulp.src('./client/blog/styles/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./client/blog/build'))
-})
+	return gulp.src('./client/blog/styles/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./client/blog/build'));
+});
 
 gulp.task('admin-sass', ['blog-sass'], ()=>{
-  return gulp.src('./client/admin/styles/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./client/admin/build'))
-})
+	return gulp.src('./client/admin/styles/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./client/admin/build'));
+});
 
 
-gulp.task('default', ['clean', 'sass'])
+gulp.task('default', ['clean', 'sass']);
