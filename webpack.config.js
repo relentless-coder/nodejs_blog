de08c4fc023c72911d69e6fe42940a061eaa34a6
relentless-coder <contact@ayushbahuguna.com>
2018-01-23 23:02:45 +0530

    set indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
 const path = require('path');
 const webpack = require('webpack');
-const PROD = process.env.NODE_ENV === 'production'
+const PROD = process.env.NODE_ENV === 'production';
 
 const blogConfiguration = {
-  entry: {
-    app: './client/blog/index.js',
-    vendor: ['axios', 'prosemirror-model', 'prosemirror-schema-basic', 'prosemirror-state']
-  },
-  module: {
-    loaders: [{
-      test: path.join(__dirname, '/client', '/blog'),
-      loader: 'babel-loader'
-    }]
-  },
-  output: {
-    filename: '[name].js',
-    path: path.join(__dirname, '/client', '/blog', '/build')
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    }),
-  ]
+	entry: {
+		app: './client/blog/index.js',
+		vendor: ['axios', 'prosemirror-model', 'prosemirror-schema-basic', 'prosemirror-state']
+	},
+	module: {
+		loaders: [{
+			test: path.join(__dirname, '/client', '/blog'),
+			loader: 'babel-loader'
+		}]
+	},
+	output: {
+		filename: '[name].js',
+		path: path.join(__dirname, '/client', '/blog', '/build')
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor'
+		}),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
+		}),
+	]
 };
 
-module.exports = [blogConfiguration]
+module.exports = [blogConfiguration];
