e2e6bcfef38c5d92543077ca0aeb15d88d384de3
relentless-coder <contact@ayushbahuguna.com>
2018-01-28 12:36:32 +0530

    on load setup editor

--- client/admin/components/component.js
+++ client/admin/components/component.js
@@ -1,7 +1,20 @@
 import {editPostHandler} from './posts/edit_post/edit.post.js';
 import {newPostHandler} from './posts/new_post/new.post.js';
 
-const post = {
-	edit: editPostHandler().editPost,
-	new: newPostHandler().createNewPost
+export const app = ()=>{
+
+    window.onload = ()=>{
+        if(document.querySelector('#new_post')){
+            newPostHandler().setupNewPost();
+        } else if(document.querySelector('#edit_post')){
+            editPostHandler().setupEditPost();
+        }
+    };
+
+    const post = {
+        edit: editPostHandler().editPost,
+        new: newPostHandler().createNewPost
+    };
+
+    return {post};
 };
