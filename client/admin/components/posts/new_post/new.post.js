61518977f3994a240f3554ba479d80e8c34899b7
relentless-coder <contact@ayushbahuguna.com>
2018-01-28 12:50:54 +0530

    plugins add basic keymap and setup menu

--- client/admin/components/posts/new_post/new.post.js
+++ client/admin/components/posts/new_post/new.post.js
@@ -1,38 +1,51 @@
-import {DOMParser} from 'prosemirror-model';
-import {schema} from 'prosemirror-schema-basic';
+import {DOMParser, Schema} from 'prosemirror-model';
+import {EditorView} from 'prosemirror-view';
 import {EditorState} from 'prosemirror-state';
+import {baseKeymap} from 'prosemirror-commands';
+import {keymap} from 'prosemirror-keymap';
+import {setupMenu, schema} from '../../editor/menu.items.js';
 import {apiHandler} from '../../../handlers/api.handler.js';
 import {errorHandler} from '../../../handlers/error.handler.js';
 
-function newPostHandler() {
-	let content = document.getElementById('new_post');
+export const newPostHandler = ()=> {
 
-	let postContent = EditorState.create({
-		doc: DOMParser.fromSchema(schema).parse(content)
-	});
-	
-	const createNewPost = ()=>{
-		const post = {
-			title: document.getElementById('post_title').value,
-			content: postContent,
-			meta: {
-				description: document.getElementById('meta_desc'),
-				author: document.getElementById('meta_author'),
-				keywords: document.getElementById('meta_keywords')
-			},
-			category: document.getElementById('post_category')
-		};
+    let content = document.querySelector('#new_post');
 
-		const options = {
-			method: 'post',
-			url: '/api/v1/post',
-			date: post,
-			config: true
-		};
 
-		return apiHandler.callAPi(options).then(console.log).catch(errorHandler);
+    const setupNewPost = ()=>{
 
-	};
+        window.view = new EditorView(document.querySelector('#editor'), {
+            state: EditorState.create({
+                doc: DOMParser.fromSchema(schema).parse(content),
+                plugins: [keymap(baseKeymap), setupMenu()]
+            })
+        });
 
-	return {createNewPost};
-}
+    };
+
+    const createNewPost = ()=>{
+
+        const post = {
+            title: document.getElementById('post_title').value,
+            content: DOMParser.fromSchema(schema).parse(content),
+            meta: {
+                description: document.getElementById('meta_desc'),
+                author: document.getElementById('meta_author'),
+                keywords: document.getElementById('meta_keywords')
+            },
+            category: document.getElementById('post_category')
+        };
+
+        const options = {
+            method: 'post',
+            url: '/api/v1/post',
+            date: post,
+            config: true
+        };
+
+        return apiHandler.callAPi(options).then(console.log).catch(errorHandler);
+
+    };
+
+    return {createNewPost, setupNewPost};
+};
