b97c606c5f1636193be4101774194bd84c02c654
relentless-coder <contact@ayushbahuguna.com>
2018-01-30 08:34:12 +0530

    sidebar menu items

---
+++ server/config/sidebar.js
@@ -0,0 +1,15 @@
+export const sidebar = [
+    {
+        'title': 'New Post',
+        'url': '/admin/new'
+    },{
+        'title': 'Home',
+        'url': '/admin'
+    },{
+        'title': 'All Posts',
+        'url': '/admin/all'
+    },{
+        'title': 'Newsletter',
+        'url': '/admin/newsletter'
+    }
+];
