6b15a0aa4c06b28eb6d7bbe2d158debf1aea0f65
relentless-coder <contact@ayushbahuguna.com>
2018-02-04 21:12:11 +0530

    render profile and updateuser routes

--- server/modules/user/user.routes.js
+++ server/modules/user/user.routes.js
@@ -5,6 +5,10 @@ let router = new Router();
 
 router.post('/signup', controller.signup);
 
+router.get('/admin/profile', controller.renderProfile);
+
+router.put('/', controller.updateUser);
+
 router.post('/signin', controller.signin);
 
 export {router as userRouter};
