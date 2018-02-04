c5e566e643f3d740b032fdccba1f0c488ba90265
relentless-coder <contact@ayushbahuguna.com>
2018-02-04 22:33:53 +0530

    no need to curry function

--- server/handlers/upload.handler.js
+++ server/handlers/upload.handler.js
@@ -1,29 +1,25 @@
 import {upload} from '../config/multer.config';
 
 
-function uploadHandler() {
-    return (req, res, key) => {
-        const uploadName = upload.file(key);
+export const uploadHandler = (req, res, key) => {
+    const uploadName = upload.single(key);
 
-        return new Promise((resolve, reject) => {
-            uploadName(req, (err) => {
-                if (err)
-                    reject(err);
-                else {
-                    let files = [];
-                    if (req.files) {
-                        req.files.forEach((file) => {
-                            files.push(file.path);
-                        });
-                        req.body.gallery = files;
-                    }
-
-                    resolve(req);
+    return new Promise((resolve, reject) => {
+        uploadName(req, res, (err) => {
+            if (err)
+                reject(err);
+            else {
+                let files = [];
+                if (req.files) {
+                    req.files.forEach((file) => {
+                        files.push(file.path);
+                    });
+                    req.body.gallery = files;
                 }
 
-            });
-        });
-    };
-}
+                resolve(req);
+            }
 
-export default uploadHandler();
+        });
+    });
+};
