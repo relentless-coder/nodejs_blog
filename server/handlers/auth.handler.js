e9366cdeba14ffda21ef11791a068c1b4cfcdf92
relentless-coder <contact@ayushbahuguna.com>
2018-02-10 08:28:45 +0530

    use responsehandler for all responses

--- server/handlers/auth.handler.js
+++ server/handlers/auth.handler.js
@@ -1,6 +1,6 @@
 import moment from 'moment-timezone';
 import {decode} from '../services/jwt/jwt.decode';
-import {findSingle} from '../services/mongodb/mongodb.service';
+import mongodb from '../services/mongodb/mongodb.service';
 import {ObjectID} from 'mongodb';
 import {parseUser} from '../services/layers/user.layer';
 import {responseHandler} from './response.handler';
@@ -10,29 +10,57 @@ function authHandler(req, res, next) {
     let payload;
     try {
         payload = decode(req);
+        console.log('payload is ', payload);
         let expTime = moment.tz(payload.exp, 'GMT').toDate();
         let currentTIme = moment.tz('GMT').toDate();
 
         if (currentTIme > expTime) {
-            return responseHandler(res, 401, 'Unauthorized request', 'The jwt token provided by the client has expired. Kindly, login again.', true);
+            const options = {
+                status: 401,
+                message: 'Unauthorized request',
+                data: 'The jwt token provided by the client has expired. Kindly, login again.',
+                content: 'application/json'
+            }
+            return responseHandler(res, options);
         } else {
             let query = {
                 _id: ObjectID(payload._id)
             };
 
-            return findSingle('users', query, parseUser).then((data) => {
+            return mongodb.findSingle('users', query, parseUser).then((data) => {
                 if (data) {
-                    res.payload = data._id;
+                    res.payload = data.data._id;
+                    console.log("res.payload is ", res.payload);
                     return next();
                 } else {
-                    return responseHandler(res, 401, 'Unauthorized request', 'User doesn\'t exist', true);
+                    const options = {
+                        status: 401,
+                        message: 'Unauthorized request',
+                        data: 'User doesn\'t exist',
+                        content: 'application/json'
+                    }
+                    return responseHandler(res, options);
                 }
             }).catch((err) => {
-                return responseHandler(res, 500, 'Sorry, we seem to be facing some issue right now. Please, try again later.', err, true);
+                console.log('auth handler is ', err);
+                const options = {
+                    status: 500,
+                    message: 'Sorry, we seem to be facing some issue right now. Please, try again later.',
+                    data: util.format(err)
+                }
+                const headers = [{name: 'Content-Type', value: 'application/json'}];
+                return responseHandler(res, options, headers);
             });
         }
     } catch (err) {
-        return responseHandler(res, err.code, err.message, err.error, true);
+        console.log('catch error is ', err)
+        const options = {
+            status: err.status ? err.status : 500,
+            message: err.message,
+            data: err.error
+        }
+        const headers = [{name: 'Content-Type', value: 'application/json'}];
+        return responseHandler(res, options, headers);
     }
 
 }
