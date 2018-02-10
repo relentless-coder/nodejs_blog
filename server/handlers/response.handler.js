800c728883da205b0ed9c03e0280677027a04b0e
relentless-coder <contact@ayushbahuguna.com>
2018-02-10 08:30:01 +0530

    expect array of headers

--- server/handlers/response.handler.js
+++ server/handlers/response.handler.js
@@ -1,10 +1,13 @@
-function responseHandler(res, options) {
-    if(!(Object.keys(options).length >= 4)){
+function responseHandler(res, options, headers) {
+    if(!(Object.keys(options).length >= 3)){
         throw SyntaxError('You need to pass atleast 4 arguments');
     } else if (typeof arguments[0] !== 'object' || !arguments[0].writeHead || !arguments[0].end) {
         throw TypeError('The first argument must be an object with methods writeHead and end');
     } else {
-        res.writeHead(options.status, {'Content-Type': options.content});
+        const localHeaders = {};
+        console.log(headers);
+        headers.forEach(el => localHeaders[el.name] = el.value)
+        res.writeHead(options.status, localHeaders);
         let data;
         if(typeof options.data === 'object')
             data = JSON.stringify(options.data);
