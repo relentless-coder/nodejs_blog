22f20f644e36881f44b750968cd6775cfe392b8d
relentless-coder <contact@ayushbahuguna.com>
2018-02-10 08:27:31 +0530

    reformat code

--- client/admin/src/handlers/api.handler.js
+++ client/admin/src/handlers/api.handler.js
@@ -1,28 +1,33 @@
 import axios from 'axios';
 import {authHandler} from './auth.handler.js';
 
-export const apiHandler = (()=>{
-    const setConfig = ()=>{
-        return {'headers': {'Authorization': `Bearer ${authHandler.setToken()}`}};
+export const apiHandler = (() => {
+    const setConfig = () => {
+        return {'Authorization': `Bearer ${authHandler.getToken()}`};
     };
 
-    const callAPi = ({method, url, data, config})=>{
+    const callAPi = ({method, url, data, config}) => {
         let localConfig;
-        if(config)
+        if (config) {
             localConfig = setConfig();
-        if(data)
+        }
+        if (data) {
+            console.log("localConfig is ", localConfig)
             return axios({
                 method,
                 url,
                 data,
+                headers: localConfig
+            });
+        } else {
+            return axios({
+                method,
+                url,
                 localConfig
             });
-		
-        return axios({
-            method,
-            url,
-            localConfig
-        });
+        }
+
+
     };
 
     return {callAPi};
