402a073b57018431aa3429b686d75f0713dda96f
relentless-coder <contact@ayushbahuguna.com>
2018-02-18 22:12:42 +0530

    use a dom element creation helper

--- client/admin/src/components/user/update/user.js
+++ client/admin/src/components/user/update/user.js
@@ -6,6 +6,18 @@ import {DOMParser} from 'prosemirror-model';
 import {baseKeymap} from 'prosemirror-commands';
 import {apiHandler} from '../../../handlers/api.handler';
 
+const createDomElement = (type, attributes) => {
+    const el = document.createElement(type);
+    if (Array.isArray(attributes)) {
+        attributes.forEach((attr) => {
+            el.setAttribute(attr.name, attr.value)
+        });
+        return el
+    }
+    el.setAttribute(attributes.name, attributes.value);
+    return el
+};
+
 export const profileHandler = () => {
 
     let content = document.querySelector('#profile_about');
@@ -23,18 +35,54 @@ export const profileHandler = () => {
 
     const addSocialProfile = () => {
         const button = document.querySelector('.add_social_profile');
-        const div = document.createElement('div');
-        div.setAttribute('class', 'social_profiles');
-        const inputName = document.createElement('input');
-        const inputLink = document.createElement('input');
-        inputName.setAttribute('type', 'text');
-        inputName.setAttribute('placeholder', 'Name of the social profile');
-        inputLink.setAttribute('type', 'text');
-        inputLink.setAttribute('placeholder', 'Link to the social profile');
+        const div = createDomElement('div', {name: 'class', value: 'social_profiles'})
+
+        const inputName = createDomElement('input', [{name: 'type', value: 'text'}, {
+            name: 'placeholder',
+            value: 'Name of the social profile'
+        }]);
+        const inputLink = createDomElement('input', [{name: 'type', value: 'text'}, {
+            name: 'placeholder',
+            value: 'Link to the social profile'
+        }]);
+
         div.appendChild(inputName);
+
         div.appendChild(inputLink);
+
         document.querySelector('.social_profiles_wrapper').insertBefore(div, button);
     };
+
+    const addHelpfulLinks = () => {
+        const button = document.querySelector('.add_helpful_links');
+        let helpful_links_wrapper;
+        if (!document.querySelector('.helpful_links_wrapper')) {
+            helpful_links_wrapper = createDomElement('div', {name: 'class', value: 'helpful_links_wrapper'});
+        } else {
+            helpful_links_wrapper = document.querySelector('.helpful_links_wrapper');
+        }
+
+        const div = createDomElement('div', {name: 'class', value: 'helpful_link'});
+
+        const linkName = createDomElement('input', [{name: 'type', value: 'text'}, {
+            name: 'placeholder',
+            value: 'Enter the title of the link'
+        }]);
+        const link = createDomElement('input', [{name: 'type', value: 'text'}, {
+            name: 'placeholder',
+            value: 'Enter the link to the resource'
+        }]);
+        const about = createDomElement('textarea', [{name: 'row', value: '5'}, {
+            name: 'col',
+            value: '10'
+        }, {name: 'class', value: 'helpful_link_about'}]);
+
+        div.appendChild(linkName);
+        div.appendChild(about);
+        div.appendChild(link);
+
+        helpful_links_wrapper.insertBefore(div, button);
+    }
 
     const updateProfile = () => {
         const form = new FormData();
@@ -45,13 +93,26 @@ export const profileHandler = () => {
         const profileWrapper = document.querySelector('.social_profiles_wrapper');
         const socialProfiles = [...profileWrapper.children];
 
-        console.log(socialProfiles);
-        console.log(socialProfiles[0].children);
+        const helpFullLinksWrapper = document.querySelector('.helpful_links_wrapper');;
+        const helpFullLinks = [...helpFullLinksWrapper.children];
+
+        let links = [];
+
+        helpFullLinks.forEach((el, i)=>{
+            if(el.nodeName !== 'BUTTON') {
+                if (el.children[0].value && el.children[1].value) {
+                    links[i] = {
+                        title: el.children[0].value,
+                        about: el.children[1].value,
+                        link: el.children[2].value
+                    };
+                }
+            }
+        })
 
         let profiles = [];
 
         socialProfiles.forEach((el, i) => {
-            console.log(el);
             if (el.nodeName !== 'BUTTON') {
                 if (el.children[0].value && el.children[1].value) {
                     profiles[i] = {
@@ -62,9 +123,8 @@ export const profileHandler = () => {
             }
         });
 
-        console.log(profiles);
-
         form.append('social', JSON.stringify(profiles));
+        form.append('projects', JSON.stringify(links));
 
         for (let pair of form) {
             console.log(pair[0], pair[1]);
@@ -81,6 +141,6 @@ export const profileHandler = () => {
 
     };
 
-    return {setupUpdateProfile, addSocialProfile, updateProfile};
+    return {setupUpdateProfile, addSocialProfile, updateProfile, addHelpfulLinks};
 
 };
