aae388fdbd815d5439baa99d973beeae6df0b1cd
relentless-coder <contact@ayushbahuguna.com>
2018-03-03 15:50:38 +0530

    send out posts

--- server/modules/module.js
+++ server/modules/module.js
@@ -7,6 +7,7 @@ import {responseHandler} from '../handlers/response.handler.js';
 import mongodb from '../services/mongodb/mongodb.service';
 import {getUser} from '../services/layers/user.layer';
 import {subsciberRouter} from './subscriber/subscriber.routes';
+import {getPost} from '../services/layers/post.layer';
 
 const router = new Router();
 
@@ -14,7 +15,7 @@ function routerFactory() {
 
     router.use('/post', postRouter);
     router.use('/user', userRouter);
-    router.use('/subscriber', subsciberRouter);
+    router.use('/subscribe', subsciberRouter);
 
     const options = {
         content: {
@@ -32,27 +33,32 @@ function routerFactory() {
     };
 
     router.get('/', (req, res)=>{
-
+        let user, posts;
         const findUser = ()=>{
             return mongodb.findAll('users', {}, getUser)
         };
+        const findPosts = ({data})=>{
+            user = data[0];
+            return mongodb.findAll('posts', {}, getPost)
+        };
         const setupRender = ({data})=>{
-            let user = data[0];
+            posts = data.splice(0, 5);
             user.social = JSON.parse(user.social);
             user.projects = JSON.parse(user.projects);
             const options = {
                 content: {
                     user,
+                    posts,
                     meta: {
                         title: 'Ayush Bahuguna | Fullstack Developer',
                         keywords: 'ayush bahuguna,fullstack developer new delhi,javascript developer new delhi,fullstack development tutorials,nodejs tutorials,mongodb tutorials,aws tutorials,freelance developer new delhi',
                         description: 'Official page of Ayush Bahuguna, who is working as fullstack developer in New Delhi, and is looking forward to exciting freelance opportunities. This website also provides helpful tutorials that may help fellow developers at work.'
                     }
                 }
-            }
+            };
             return  renderView('blog/src/components/home/home.ejs', options)
         }
-        findUser().then(setupRender).then((str)=>{
+        findUser().then(findPosts).then(setupRender).then((str)=>{
             let options = {
                 status: 200,
                 message: 'Success',
