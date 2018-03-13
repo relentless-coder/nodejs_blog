64c4bc24d7af09096cb6c580b15ef3841e70ae36
relentless-coder <contact@ayushbahuguna.com>
2018-03-13 23:25:48 +0530

    config file to read sensitive data

---
+++ server/config/package-config.js
@@ -0,0 +1,7 @@
+export const config = {
+    secret: process.env.SECRET,
+    host: process.env.mailHost,
+    port: process.env.mailPort,
+    mailUser: process.env.mailUser,
+    mailPassword: process.env.mailPassword
+};
