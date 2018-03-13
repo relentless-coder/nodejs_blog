8219848561ec5ab36008c627ed274b6a96998e91
relentless-coder <contact@ayushbahuguna.com>
2018-03-13 23:28:15 +0530

    delete unecessary code

--- server/modules/post/blog/post.routes.js
+++
@@ -1,7 +0,0 @@
-import Router from 'router';
-import {authHandler} from '../../../handlers/auth.handler';
-import * as controller from '../post.controllers';
-
-const router = new Router();
-
-router.post('/', authHandler, controller.addOnePost);
