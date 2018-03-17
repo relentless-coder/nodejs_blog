c4a7a0bd7fa05abded29b34e2913b6de162e9807
relentless-coder <contact@ayushbahuguna.com>
2018-03-17 23:56:47 +0530

    if after page load, height > 800, hide header

--- client/blog/src/components/posts/single_post/single.post.js
+++ client/blog/src/components/posts/single_post/single.post.js
@@ -1,18 +1,25 @@
 function singlePostHandler() {
     const post_header = document.querySelector('.heading_wrapper');
+    if (window.scrollY > 800) {
+        if (!post_header.classList.contains('hide')) {
+            post_header.classList.add('hide');
+        }
+    }
     window.addEventListener('scroll', (e) => {
         e.preventDefault();
         if (window.scrollY > 800) {
-            if (!post_header.classList.contains('hide'))
+            if (!post_header.classList.contains('hide')) {
                 post_header.classList.add('hide');
+            }
         } else {
-            if (post_header.classList.contains('hide'))
+            if (post_header.classList.contains('hide')) {
                 post_header.classList.remove('hide');
+            }
         }
 
     });
     const codeBlocks = document.querySelectorAll('pre code');
-    codeBlocks.forEach((el)=>{
+    codeBlocks.forEach((el) => {
         hljs.highlightBlock(el);
     });
 }
