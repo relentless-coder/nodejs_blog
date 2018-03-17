77f7744a47fce91797a2357ba8ea0c0f35f6df56
relentless-coder <contact@ayushbahuguna.com>
2018-03-17 13:08:36 +0530

    update controller functions to use latest responseHandler
    adapt header changes that were made to the responseHandler method

--- server/modules/post/post.controllers.js
+++ server/modules/post/post.controllers.js
@@ -8,6 +8,7 @@ import {ErrorWithStatusCode} from '../../handlers/errorhandler';
 import {responseHandler} from '../../handlers/response.handler';
 import {addComment, getComment} from '../../services/layers/comment.layer';
 import {renderView} from '../../handlers/render.view.js';
+import {uploadHandler} from '../../handlers/upload.handler.js';
 
 const sanitizeOpt = {
     allowedTags: ['img', 'p', 'pre', 'code', 'strong', 'em'],
@@ -26,7 +27,7 @@ export function getAllPosts(req, res) {
         };
     }
     return mongo.findAll('posts', query, getPost).then((data) => {
-       return renderView('blog/src/components/posts/all_posts/all.post.ejs', {content: {post: data.data, meta: {title: 'Posts | Ayush Bahuguna', description: 'Here you can find tutorials on web development topics that are much more relevant to your professional career', keywords: 'nodejs tutorials, mongodb tutorials, javascript tutorials'}}}).then((str)=>{
+        return renderView('blog/src/components/posts/all_posts/all.post.ejs', {content: {post: data.data, meta: {title: 'Posts | Ayush Bahuguna', description: 'Here you can find tutorials on web development topics that are much more relevant to your professional career', keywords: 'nodejs tutorials, mongodb tutorials, javascript tutorials'}}}).then((str)=>{
             const options = {
                 status: data.status,
                 message: data.message,
@@ -73,27 +74,35 @@ export function addOnePost(req, res) {
         if (req.body) {
             return Promise.resolve(req);
         } else {
-            return fileUpload(req, res, 'image');
+            return uploadHandler(req, res, 'image');
         }
     };
 
     const addToDatabase = () => {
         req.body.content = sanitize(req.body.content, sanitizeOpt);
         return mongo.insert('posts', req.body, addPost, getPost).then((data) => {
-            return responseHandler(res, data.status, data.message, data.data);
+            let options = {
+                status: data.status,
+                message: data.message,
+                data: data.data
+            };
+            const headers = [{name: 'Content-Type', value: 'text/html'}];
+            return responseHandler(res, options, headers);
         }).catch((err) => {
             if (err.error) {
-                throw new ErrorWithStatusCode(err.code, err.message, err.error);
+                throw new ErrorWithStatusCode(err.status, err.message, err.error);
             }
         });
     };
 
     return getData().then(addToDatabase).catch((err) => {
-        if (err.code && err.message) {
-            return responseHandler(res, err.code, err.message, err.error, true);
-        } else {
-            return responseHandler(res, 500, 'Sorry, we are facing some issue right now. Please, try again later.', err, true);
-        }
+        let options = {
+            status: err.status ? err.status : 500,
+            message: err.message ? err.message : 'Sorry, we are facing some issue right now.',
+            data: err
+        };
+        const headers = [{name: 'Content-Type', value: 'application/json'}];
+        return responseHandler(res, options, headers);
     });
 }
 
@@ -104,7 +113,7 @@ export function updateOnePost(req, res) {
             return Promise.resolve(req);
         } else {
             return new Promise((resolve, reject) => {
-                fileUpload(req, res, (err) => {
+                uploadHandler(req, res, (err) => {
                     if (err) {
                         reject(err);
                     } else {
@@ -127,29 +136,26 @@ export function updateOnePost(req, res) {
         return mongo.update('posts', {
             _id: ObjectID(req.params.postId)
         }, req.body, updatePost, getPost).then((data) => {
-            let links = [];
-            links.push({
-                href: `https://api.ayushbahuguna.com/api/v1/posts/${data.data._id}`,
-                rel: 'self',
-                method: 'GET'
-            }, {
-                href: `https://api.ayushbahuguna.com/api/v1/posts/${data.data._id}`,
-                rel: 'remove',
-                method: 'DELETE'
-            });
-            data.data.links = links;
-            return responseHandler(res, data.status, data.message, data.data);
+            let options = {
+                status: data.status,
+                message: data.message,
+                data: data.data
+            };
+            const headers = [{name: 'Content-Type', value: 'text/html'}];
+            return responseHandler(res, options, headers);
         }).catch((err) => {
-            throw new ErrorWithStatusCode(err.code, err.message, err.error);
+            throw new ErrorWithStatusCode(err.status, err.message, err.error);
         });
     };
 
     return getData().then(addToDatabase).catch((err) => {
-        if (err.code && err.message) {
-            return responseHandler(res, err.code, err.message, err.error, true);
-        } else {
-            return responseHandler(res, 500, 'Sorry, we are facing some issue right now. Please, try again later.', err, true);
-        }
+        let options = {
+            status: err.status ? err.status : 500,
+            message: err.message ? err.message : 'Sorry, we are facing some issue right now.',
+            data: err
+        };
+        const headers = [{name: 'Content-Type', value: 'application/json'}];
+        return responseHandler(res, options, headers);
     });
 }
 
@@ -159,20 +165,21 @@ export function removeOnePost(req, res) {
     };
 
     return mongo.removeOne('posts', query).then((data) => {
-        let links = [];
-        links.push({
-            href: 'https://api.ayushbahuguna.com/api/v1/posts',
-            rel: 'list',
-            method: 'GET'
-        }, {
-            href: 'https://api.ayushbahuguna.com/api/v1/posts',
-            rel: 'add',
-            method: 'POST'
-        });
-        data.data.links = links;
-        return responseHandler(res, data.status, data.message, data.data);
+        let options = {
+            status: data.status,
+            message: data.message,
+            data: data.data
+        };
+        const headers = [{name: 'Content-Type', value: 'text/html'}];
+        return responseHandler(res, options, headers);
     }).catch((err) => {
-        return responseHandler(res, err.code, err.message, err.error, true);
+        let options = {
+            status: err.status ? err.status : 500,
+            message: err.message ? err.message : 'Sorry, we are facing some issue right now.',
+            data: err
+        };
+        const headers = [{name: 'Content-Type', value: 'application/json'}];
+        return responseHandler(res, options, headers);
     });
 }
 
@@ -224,22 +231,21 @@ export function addOneComment(req, res) {
         let options = {
             status: data.status,
             message: 'Comment posted successfully',
-            data: createdComment,
-            content: 'application/json'
+            data: createdComment
         };
 
-        return responseHandler(res, options);
+        const headers = [{name: 'Content-Type', value: 'application/json'}];
+        return responseHandler(res, options, headers);
     };
 
     return getRequestBody().then(findPost).then(createComment).then(updatePostWithComment).then(sendResponse).catch((err)=>{
-        console.log('error is ', err);
         let options = {
-            status: err.status || 500,
-            message: err.message || 'Sorry, we are facing some issue right now. Please, try again later',
-            data: err.error,
-            content: 'application/json'
+            status: err.status ? err.status : 500,
+            message: err.message ? err.message : 'Sorry, we are facing some issue right now.',
+            data: err
         };
-        return responseHandler(res, options);
+        const headers = [{name: 'Content-Type', value: 'application/json'}];
+        return responseHandler(res, options, headers);
     });
 
 }
@@ -263,26 +269,27 @@ export function renderNewPost(req, res){
     };
 
     const sendResponse = (str)=>{
+        
         let options = {
             data: str,
             status: 200,
-            message: 'Serving new post template',
-            content: 'text/html'
+            message: 'Serving new post template'
         };
 
-        return responseHandler(res, options);
+        const headers = [{name: 'Content-Type', value: 'text/html'}];
+        return responseHandler(res, options, headers);
     };
 
     return getView().then(sendResponse).catch((err)=>{
-        console.log(err);
+        
         let options = {
             status: 500,
             message: 'Sorry, we are facing some issue right now. Please, try again later.',
-            data: err.error,
-            content: 'json/application'
+            data: err.error
         };
 
-        return responseHandler(res, options);
+        const headers = [{name: 'Content-Type', value: 'application/json'}];
+        return responseHandler(res, options, headers);
 
     });
 }
