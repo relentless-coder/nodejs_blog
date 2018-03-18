05e8343a7ca2c99757e4e210cc09212335e0aeda
relentless-coder <contact@ayushbahuguna.com>
2018-03-18 22:31:33 +0530

    update the apis

--- client/blog/src/components/comments/comment.factory.js
+++ client/blog/src/components/comments/comment.factory.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 function commentFactory() {
-    const postComment = (comment, url)=>{
-        console.log('comment is ', comment);
-        return axios.post(`/post/${url}/comment`, comment);
+    const postComment = (comment, postId)=>{
+        return axios.post(`/post/${postId}/comment`, comment);
     };
 
-    const replyComment = (data, id, url)=>{
-        return axios.post(`/post/${url}/comment/${id}`, data);
+    const replyComment = (data, postId, commentId)=>{
+        return axios.post(`/post/${postId}/comment/${commentId}`, data);
     };
 
     return {
