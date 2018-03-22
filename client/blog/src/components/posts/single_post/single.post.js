3f013d3816582b341368bac66efed9c61abfe5c4
relentless-coder <contact@ayushbahuguna.com>
2018-03-22 22:31:50 +0530

    no need to hide header

--- client/blog/src/components/posts/single_post/single.post.js
+++ client/blog/src/components/posts/single_post/single.post.js
@@ -1,23 +1,5 @@
 function singlePostHandler() {
     const post_header = document.querySelector('.heading_wrapper');
-    if (window.scrollY > 800) {
-        if (!post_header.classList.contains('hide')) {
-            post_header.classList.add('hide');
-        }
-    }
-    window.addEventListener('scroll', (e) => {
-        e.preventDefault();
-        if (window.scrollY > 800) {
-            if (!post_header.classList.contains('hide')) {
-                post_header.classList.add('hide');
-            }
-        } else {
-            if (post_header.classList.contains('hide')) {
-                post_header.classList.remove('hide');
-            }
-        }
-
-    });
     const codeBlocks = document.querySelectorAll('pre code');
     codeBlocks.forEach((el) => {
         hljs.highlightBlock(el);
